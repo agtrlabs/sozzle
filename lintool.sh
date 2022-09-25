2f40e9f91a3af3fe6039b0c68551034d96c4219b
Yunus AK <yunus.ak.gen.tr@gmail.com>
2022-09-25 21:14:28 +0300

    lintool

--- lintool.sh
+++ lintool.sh
@@ -6,6 +6,8 @@ exit_message () {
 
 flutter format --set-exit-if-changed lib test || exit_message
 flutter analyze lib test || exit_message
-flutter test --no-pub --coverage --test-randomize-ordering-seed random || exit_message
+flutter test -j 4 --no-pub --coverage --test-randomize-ordering-seed random || exit_message
 
+coverage_value=$(genhtml coverage/lcov.info -o coverage/ | grep -Eoh '[0-9]+.[0-9]+%')
+echo "Coverage Value = ${coverage_value}"
 echo "\e[1;42m It's ready to go!\e[0m 🚀"
