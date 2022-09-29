e417cb4818f215bc4285e415839d5f0ef673baa5
Yunus AK <yunus.ak.gen.tr@gmail.com>
2022-09-30 00:58:41 +0300

    add repository provider

--- lib/app.dart
+++ lib/app.dart
@@ -8,42 +8,54 @@ import 'package:sozzle/l10n/l10n.dart';
 import 'package:sozzle/src/apploader/application/apploader_repository.dart';
 import 'package:sozzle/src/apploader/cubit/apploader_cubit.dart';
 import 'package:sozzle/src/level/application/level_repository.dart';
+import 'package:sozzle/src/level/domain/i_level_repository.dart';
 import 'package:sozzle/src/theme/cubit/theme_cubit.dart';
+import 'package:sozzle/src/user_stats/user_stats.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
+    return MultiRepositoryProvider(
       providers: [
-        BlocProvider(
-          create: (context) => ThemeCubit(),
+        RepositoryProvider<ILevelRepository>(
+          create: (context) => LevelRepository(),
         ),
-        BlocProvider(
-          create: (context) => ApploaderCubit(
-            apploaderRepository: MockApploaderRepository(
-              LevelRepository(),
-            ),
-          ),
+        RepositoryProvider<IUserStatsRepository>(
+          create: (context) => UserStatsRepository(),
         ),
       ],
-      child: MaterialApp.router(
-        routeInformationProvider: router.routeInformationProvider,
-        routeInformationParser: router.routeInformationParser,
-        routerDelegate: router.routerDelegate,
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-          colorScheme: ColorScheme.fromSwatch(
-            accentColor: const Color(0xFF13B9FF),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => ThemeCubit(),
           ),
-        ),
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
+          BlocProvider(
+            create: (context) => ApploaderCubit(
+              apploaderRepository: MockApploaderRepository(
+                context.read<ILevelRepository>(),
+              ),
+            ),
+          ),
         ],
-        supportedLocales: AppLocalizations.supportedLocales,
+        child: MaterialApp.router(
+          routeInformationProvider: router.routeInformationProvider,
+          routeInformationParser: router.routeInformationParser,
+          routerDelegate: router.routerDelegate,
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+            colorScheme: ColorScheme.fromSwatch(
+              accentColor: const Color(0xFF13B9FF),
+            ),
+          ),
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+          ],
+          supportedLocales: AppLocalizations.supportedLocales,
+        ),
       ),
     );
   }
