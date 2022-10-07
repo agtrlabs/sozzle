2a89ac79ab775e8f9dc294e7eb16e47b1ce0da05
Yunus AK <yunus.ak.gen.tr@gmail.com>
2022-10-07 22:52:09 +0300

    add dummy data

--- lib/src/game_play/view/components/game_play_board.dart
+++ lib/src/game_play/view/components/game_play_board.dart
@@ -5,6 +5,6 @@ class GamePlayBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Text('Play Board!');
   }
 }
--- lib/src/game_play/view/components/game_play_letters.dart
+++ lib/src/game_play/view/components/game_play_letters.dart
@@ -5,6 +5,6 @@ class GamePlayLetters extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Text('Letters');
   }
 }
