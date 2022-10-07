237791b3e2f9ef80e62166d05e2cd30cf4a5a54e
Yunus AK <yunus.ak.gen.tr@gmail.com>
2022-10-08 01:42:33 +0300

    add gam_play_bloc emits wordFound, alreadyFound

--- lib/src/game_play/bloc/game_play_bloc.dart
+++ lib/src/game_play/bloc/game_play_bloc.dart
@@ -11,8 +11,76 @@ class GamePlayBloc extends Bloc<GamePlayEvent, GamePlayState> {
     on<GamePlayEventInputWord>((event, emit) {
       if (!levelData.words.contains(event.word)) {
         emit(const GamePlayState(GamePlayActualState.notFound));
+      } else {
+        // scan the board
+        final indexList = _scan(event.word);
+        // if word found
+        if (indexList.length == event.word.length) {
+          //check if alreadyFound
+          if (foundWords.contains(event.word)) {
+            emit(const GamePlayState(GamePlayActualState.alreadyFound));
+          } else {
+            // else add to foundWords
+            foundWords.add(event.word);
+            emit(
+              GamePlayState(GamePlayActualState.wordFound, indexList),
+            );
+          }
+        }
       }
     });
   }
   final LevelData levelData;
+
+  List<String> foundWords = [];
+
+  /// scans board data letf to right
+  /// if word is found return indexes of letters
+  List<int> _scanLR(String word) {
+    var result = <int>[];
+    for (var row = 0; row < levelData.boardHeight; row++) {
+      var wordPointer = 0;
+      result = [];
+      for (var col = 0; col < levelData.boardWidth; col++) {
+        //check current cell matches with wordPointer
+        final currentIndex = row * levelData.boardWidth + col;
+        if (levelData.boardData[currentIndex] == word[wordPointer]) {
+          result.add(currentIndex);
+
+          if (wordPointer == word.length - 1) return result;
+          wordPointer++;
+        } else {
+          wordPointer = 0;
+          result = [];
+        }
+      }
+    }
+    return result;
+  }
+
+  // scan each column top to down to find the word
+  List<int> _scanTD(String word) {
+    var result = <int>[];
+    for (var col = 0; col < levelData.boardWidth; col++) {
+      var wordPointer = 0;
+      result = [];
+      for (var row = 0; row < levelData.boardHeight; row++) {
+        //check current cell matches with wordPointer
+        final currentIndex = row * levelData.boardWidth + col;
+        if (levelData.boardData[currentIndex] == word[wordPointer]) {
+          result.add(currentIndex);
+          if (wordPointer == word.length - 1) return result;
+          wordPointer++;
+        } else {
+          result = [];
+        }
+      }
+    }
+    return result;
+  }
+
+  List<int> _scan(String word) {
+    final list = _scanLR(word);
+    return list.length == word.length ? list : _scanTD(word);
+  }
 }
--- test/src/game_play/bloc/game_play_bloc_test.dart
+++ test/src/game_play/bloc/game_play_bloc_test.dart
@@ -3,18 +3,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sozzle/src/game_play/bloc/game_play_bloc.dart';
 import 'package:sozzle/src/level/domain/level_data.dart';
 
+/*
+EXIST
+    E
+    S
+    T
+*/
 LevelData levelData = LevelData(
-  boardData: [],
+  boardData: [
+    ...'     '.split(''),
+    ...'EXIST'.split(''),
+    ...'    E'.split(''),
+    ...'    S'.split(''),
+    ...'    T'.split(''),
+  ],
   boardWidth: 5,
-  boardHeight: 3,
+  boardHeight: 5,
   levelId: 1,
-  words: ['TEST'],
+  words: ['EXIST', 'TEST'],
 );
 
 void main() {
   group('GamePlayBloc', () {
     late GamePlayBloc bloc;
-
     setUp(() {
       bloc = GamePlayBloc(levelData: levelData);
     });
@@ -28,10 +39,45 @@ void main() {
     });
 
     blocTest<GamePlayBloc, GamePlayState>(
-      'emits state notFound if word not exist',
+      'emits state notFound if word does not exist',
       build: () => bloc,
       act: (bloc) => bloc.add(GamePlayEventInputWord('NOTEXIST')),
-      verify: (e) => expect(e.state.state, GamePlayActualState.notFound),
+      expect: () => [const GamePlayState(GamePlayActualState.notFound)],
+      //verify: (e) => expect(e.state.state, GamePlayActualState.notFound),
+    );
+
+    blocTest<GamePlayBloc, GamePlayState>(
+      'emits state wordFound with cell List, if word exists left to right',
+      build: () => bloc,
+      act: (bloc) => bloc.add(GamePlayEventInputWord('EXIST')),
+      verify: (e) {
+        expect(e.state.state, GamePlayActualState.wordFound);
+        expect(e.state.revealedCells, [5, 6, 7, 8, 9]);
+      },
+    );
+
+    blocTest<GamePlayBloc, GamePlayState>(
+      'emits state wordFound with cell List, if word exists top to down',
+      build: () => bloc,
+      act: (bloc) => bloc.add(GamePlayEventInputWord('TEST')),
+      verify: (e) {
+        expect(e.state.state, GamePlayActualState.wordFound);
+        expect(e.state.revealedCells, [9, 14, 19, 24]);
+      },
+    );
+
+    blocTest<GamePlayBloc, GamePlayState>(
+      'emits state alreadyFound if added twice',
+      build: () => GamePlayBloc(levelData: levelData),
+      act: (bloc) {
+        bloc
+          ..add(GamePlayEventInputWord('EXIST'))
+          ..add(GamePlayEventInputWord('EXIST'));
+      },
+      skip: 1,
+      //wait: Duration(milliseconds: 200),
+      expect: () => [const GamePlayState(GamePlayActualState.alreadyFound)],
+      //verify: (e) => expect(e.state.state, GamePlayActualState.alreadyFound),
     );
   });
 }
