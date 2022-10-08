4dace6576a5db6da8d990cdaef14d40b95372363
Yunus AK <yunus.ak.gen.tr@gmail.com>
2022-10-08 19:28:06 +0300

    add some screen objects

--- lib/src/game_play/view/components/game_play_board.dart
+++ lib/src/game_play/view/components/game_play_board.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sozzle/src/theme/cubit/theme_cubit.dart';
 
 class GamePlayBoard extends StatelessWidget {
   const GamePlayBoard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Play Board!');
+    final theme = BlocProvider.of<ThemeCubit>(context).state;
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        color: theme.backgroundColor,
+        child: Text(
+          'Play Board!',
+          style: TextStyle(color: theme.primaryTextColor),
+        ),
+      ),
+    );
   }
 }
--- lib/src/game_play/view/components/game_play_header.dart
+++ lib/src/game_play/view/components/game_play_header.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sozzle/src/theme/cubit/theme_cubit.dart';
+import 'package:sozzle/src/user_stats/cubit/user_stats_cubit.dart';
 
 class GamePlayHeader extends StatelessWidget {
   const GamePlayHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Header');
+    return BlocBuilder<UserStatsCubit, UserStatsState>(
+      builder: (context, state) {
+        final theme = BlocProvider.of<ThemeCubit>(context).state;
+        return AppBar(
+          backgroundColor: theme.backgroundColor,
+          leading: IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () {
+              GoRouter.of(context).replace('/home');
+            },
+          ),
+          centerTitle: true,
+          title: Text('Level ${state.progress.currentLevel}'),
+        );
+      },
+    );
   }
 }
