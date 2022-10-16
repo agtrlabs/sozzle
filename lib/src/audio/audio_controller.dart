c07db2fbfb76167a12c6ea864b9d88da5522f4df
Yunus AK <yunus.ak.gen.tr@gmail.com>
2022-10-16 22:05:46 +0300

    add audio controller

---
+++ lib/src/audio/audio_controller.dart
@@ -0,0 +1,21 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:sozzle/src/audio/domain/i_audio_controller.dart';
+import 'package:sozzle/src/audio/domain/sfx.dart';
+import 'package:sozzle/src/settings/cubit/setting_cubit.dart';
+
+class AudioController implements IAudioController {
+  AudioController({required SettingState settings}) : _settings = settings;
+
+  late final SettingState _settings;
+  final player = AudioPlayer();
+
+  @override
+  SettingState get settings => _settings;
+
+  @override
+  void play(Sfx sfx) {
+    if (!_settings.isMute && _settings.isSoundOn) {
+      player.play(DeviceFileSource(sound[sfx]!));
+    }
+  }
+}
---
+++ lib/src/audio/domain/i_audio_controller.dart
@@ -0,0 +1,10 @@
+import 'package:sozzle/src/audio/domain/sfx.dart';
+import 'package:sozzle/src/settings/cubit/setting_cubit.dart';
+
+abstract class IAudioController {
+  IAudioController({required this.settings});
+
+  final SettingState settings;
+
+  void play(Sfx sfx);
+}
