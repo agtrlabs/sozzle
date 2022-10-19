8db18925bf8643f59262b18480dcf5eae0f368ba
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2022-10-19 11:04:13 +0000

    feat: added animation to the splash screen

--- lib/src/splash/splash_page.dart
+++ lib/src/splash/splash_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -5,14 +8,60 @@ import 'package:sozzle/l10n/l10n.dart';
 import 'package:sozzle/src/apploader/cubit/apploader_cubit.dart';
 import 'package:sozzle/src/theme/cubit/theme_cubit.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
   static const String path = '/';
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  bool a = false;
+  bool b = false;
+  bool c = false;
+  bool d = false;
+  bool e = false;
+  int counter = 0;
+  static const colorizeColors = [
+    Colors.purple,
+    Colors.blue,
+    Colors.yellow,
+    Colors.red,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(milliseconds: 100), (timer) {
+      counter += 100;
+      switch (counter) {
+        case 400:
+          a = true;
+          b = true;
+          break;
+        case 1300:
+          c = true;
+          break;
+        case 1700:
+          e = true;
+          break;
+        case 3400:
+          d = true;
+          timer.cancel();
+          break;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     final apploader = context.read<ApploaderCubit>();
+    final mq = MediaQuery.of(context);
+    final height = mq.size.height;
     return BlocBuilder<ThemeCubit, ThemeState>(
       builder: (context, state) {
         return Scaffold(
@@ -21,13 +70,51 @@ class SplashPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  'Sozzle',
-                  style: TextStyle(
-                    color: state.primaryTextColor,
-                    fontSize: 24,
-                  ),
+                AnimatedContainer(
+                  duration: Duration(milliseconds: d ? 900 : 2500),
+                  curve: d ? Curves.fastLinearToSlowEaseIn : Curves.elasticOut,
+                  height: d
+                      ? 0
+                      : a
+                      ? height / 2
+                      : 20,
+                  width: 20,
                 ),
+                if (d)
+                  Text(
+                    'Sozzle',
+                    style: TextStyle(
+                      color: state.primaryTextColor,
+                      fontSize: 24,
+                    ),
+                  )
+                else
+                  AnimatedContainer(
+                    duration: Duration(seconds: c ? 2 : 0),
+                    curve: Curves.fastLinearToSlowEaseIn,
+                    height: c ? 80 : 20,
+                    width: c ? 200 : 20,
+                    decoration: BoxDecoration(
+                      color: b ?  Colors.white : Colors.transparent,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: e
+                        ? Center(
+                      child: AnimatedTextKit(
+                        totalRepeatCount: 1,
+                        animatedTexts: [
+                          ColorizeAnimatedText(
+                            'Sozzle',
+                            colors: colorizeColors,
+                            textStyle: const TextStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                        : const SizedBox(),
+                  ),
                 Padding(
                   padding: const EdgeInsets.all(20),
                   child: BlocBuilder<ApploaderCubit, ApploaderState>(
@@ -35,19 +122,27 @@ class SplashPage extends StatelessWidget {
                       if (state.loaderState == LoaderState.loadingPuzzle) {
                         return LinearProgressIndicator(
                           value: state.percent / 100,
+                          color: d ? null : Colors.transparent,
+                          backgroundColor: d ? null : Colors.transparent,
                         );
                       } else if (state.loaderState ==
                           LoaderState.loadingUserData) {
                         return LinearProgressIndicator(
                           value: state.percent / 100,
+                          color: d ? null : Colors.transparent,
+                          backgroundColor: d ? null : Colors.transparent,
                         );
                       } else if (state.loaderState ==
                           LoaderState.loadingSettingsData) {
                         return LinearProgressIndicator(
                           value: state.percent / 100,
+                          color: d ? null : Colors.transparent,
+                          backgroundColor: d ? null : Colors.transparent,
                         );
                       } else if (state.loaderState == LoaderState.loaded) {
-                        return const StartButton();
+                        return StartButton(
+                          textColour: d ? null : Colors.transparent,
+                        );
                       }
                       apploader.updatePuzzleData();
                       return Container();
@@ -66,7 +161,10 @@ class SplashPage extends StatelessWidget {
 class StartButton extends StatelessWidget {
   const StartButton({
     super.key,
+    this.textColour,
   });
+
+  final Color? textColour;
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +172,7 @@ class StartButton extends StatelessWidget {
     return TextButton(
       child: Text(
         l10n.startButton,
-        style: const TextStyle(fontSize: 24),
+        style: TextStyle(fontSize: 24, color: textColour),
       ),
       onPressed: () {
         GoRouter.of(context).replace('/home');
