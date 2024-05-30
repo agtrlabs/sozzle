14daae4141314bf107d1914df9ee4dd11e7697cd
NonymousMorlock <agamspaul@gmail.com>
2024-05-30 23:57:47 +0000

    ✅ Build configuration change: Removed the DevicePreview

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,5 +1,3 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:sozzle/app.dart';
@@ -8,11 +6,6 @@ import 'package:sozzle/bootstrap.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  bootstrap(
-    () => DevicePreview(
-      enabled: defaultTargetPlatform != TargetPlatform.iOS &&
-          defaultTargetPlatform != TargetPlatform.android,
-      builder: (_) => const App(),
-    ),
-  );
+
+  bootstrap(() => const App());
 }
