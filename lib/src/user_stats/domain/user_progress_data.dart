8a56fcb6c309c6c69c3d668118cef09ab6f45bdc
NonymousMorlock <agamspaul@gmail.com>
2024-06-12 00:15:12 +0000

    ✨ Feat: Add hintCount property to user progress data for hint feature

--- lib/src/user_stats/domain/user_progress_data.dart
+++ lib/src/user_stats/domain/user_progress_data.dart
@@ -1,12 +1,19 @@
 /// Current Users progress data
 class UserProgressData {
-  UserProgressData({required this.currentLevel});
+  UserProgressData({required this.currentLevel, this.hintCount = 0});
 
   factory UserProgressData.fromMap(Map<String, dynamic> json) =>
-      UserProgressData(currentLevel: json['currentLevel'] as int);
+      UserProgressData(
+        currentLevel: json['currentLevel'] as int,
+        hintCount: json['hintCount'] as int,
+      );
 
   /// max level user reached
   int currentLevel;
+  int hintCount;
 
-  Map<String, dynamic> toMap() => {'currentLevel': currentLevel};
+  Map<String, dynamic> toMap() => {
+        'currentLevel': currentLevel,
+        'hintCount': hintCount,
+      };
 }
