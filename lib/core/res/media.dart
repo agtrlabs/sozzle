50e6e25ebbe7c553a10a03e4495ac48d751c9a25
NonymousMorlock <agamspaul@gmail.com>
2024-06-17 22:57:07 +0000

    ✨ Feat: Create a bucket for Game Media Assets

---
+++ lib/core/res/media.dart
@@ -0,0 +1,12 @@
+sealed class Media {
+  const Media();
+  static const _baseRive = 'assets/rive';
+  static const _baseIcons = 'assets/icons';
+
+  // Icons
+  static const hint = '$_baseIcons/bulb.png';
+
+  // Rive
+  static const lake = '$_baseRive/small_lake_on_a_rainy_day.riv';
+  static const space = '$_baseRive/space.riv';
+}
