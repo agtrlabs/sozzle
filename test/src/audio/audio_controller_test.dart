481a58f12ee77bd1d2722cfc4d65927f47277124
NonymousMorlock <agamspaul@gmail.com>
2024-06-18 11:09:42 +0000

    Add game button rive animation asset

--- test/src/audio/audio_controller_test.dart
+++ test/src/audio/audio_controller_test.dart
@@ -1,23 +1,25 @@
-import 'package:bloc_test/bloc_test.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mocktail/mocktail.dart';
-// import 'package:sozzle/src/audio/audio_controller.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:sozzle/src/audio/audio_controller.dart';
 import 'package:sozzle/src/settings/cubit/setting_cubit.dart';
 
-class MockSettingsCubit extends MockCubit<SettingState>
-    implements SettingCubit {}
+import '../../helpers/helpers.dart';
 
 void main() {
-/*  TestWidgetsFlutterBinding.ensureInitialized();
-  test('audio controller ...', () {
-    late MockSettingsCubit settingsCubit;
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  late MockSettingsCubit settingsCubit;
+  late AudioController audioController;
+  setUp(() {
     settingsCubit = MockSettingsCubit();
     when(() => settingsCubit.state).thenReturn(SettingState.initial());
-    final audioController = AudioController(
+    audioController = AudioController(
       settingsCubit: settingsCubit,
       initialSettings: SettingState.initial(),
     );
+  });
+
+  test('audio controller ...', () {
     expect(audioController.settings, settingsCubit.state);
   });
-*/
 }
