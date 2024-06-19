8a37221d67f3f1c9f09db454e40469377b86b70e
NonymousMorlock <agamspaul@gmail.com>
2024-06-19 11:28:11 +0000

    ✅ Build configuration change: Expose environment to the dev space

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,10 +2,12 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:sozzle/app.dart';
 import 'package:sozzle/bootstrap.dart';
+import 'package:sozzle/core/environment.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 
+  Environment.instance.setEnvironment(EnvironmentType.DEVELOPMENT);
   bootstrap(() => const App());
 }
