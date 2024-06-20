edf24f182ec4caa4daca6df144da54e5865a9313
NonymousMorlock <agamspaul@gmail.com>
2024-06-20 11:39:27 +0000

    ✨ Feat: Implement cache functionality for sound and music to be restored to last known state prior to mute being turned on

--- lib/src/settings/cubit/setting_cubit.dart
+++ lib/src/settings/cubit/setting_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sozzle/src/settings/domain/i_setting_repository.dart';
+import 'package:sozzle/src/settings/domain/settings.dart';
 import 'package:sozzle/src/theme/cubit/theme_cubit.dart';
 import 'package:sozzle/utilities/settings_helper.dart';
 
@@ -29,9 +29,10 @@ class SettingCubit extends Cubit<SettingState> {
   Future<void> toggleSoundOption({required bool val}) async {
     final newState = state.copyWith(
       isSoundOn: val,
-      isMute: SettingsHelper.turnOffMuteMaybe(val: val),
+      isMute: !state.isMusicOn && !val,
     );
-    await settingRep.setSoundSetting(value: val);
+
+    await settingRep.setSoundSetting(value: val, cache: true);
     await settingRep.setMuteSetting(value: newState.isMute);
     emit(newState);
   }
@@ -39,9 +40,9 @@ class SettingCubit extends Cubit<SettingState> {
   Future<void> toggleMusicOption({required bool val}) async {
     final newState = state.copyWith(
       isMusicOn: val,
-      isMute: SettingsHelper.turnOffMuteMaybe(val: val),
+      isMute: !state.isSoundOn && !val,
     );
-    await settingRep.setMusicSetting(value: val);
+    await settingRep.setMusicSetting(value: val, cache: true);
     await settingRep.setMuteSetting(
       value: newState.isMute,
     );
@@ -62,18 +63,21 @@ class SettingCubit extends Cubit<SettingState> {
   }
 
   Future<void> toggleMuteOption({required bool val}) async {
+    Settings? cache;
+    if (!val) {
+      cache = await settingRep.getSetting();
+    }
     final newState = state.copyWith(
       isMute: val,
-      isMusicOn: SettingsHelper.turnOffSoundAndMusicMaybe(val: val),
-      isSoundOn: SettingsHelper.turnOffSoundAndMusicMaybe(val: val),
+      isMusicOn: SettingsHelper.turnOffSoundAndMusicMaybe(val: val) ??
+          cache!.isMusicOnCache,
+      isSoundOn: SettingsHelper.turnOffSoundAndMusicMaybe(val: val) ??
+          cache!.isSoundOnCache,
     );
+
     await settingRep.setMuteSetting(value: val);
-    await settingRep.setMusicSetting(
-      value: newState.isMusicOn,
-    );
-    await settingRep.setSoundSetting(
-      value: newState.isSoundOn,
-    );
+    await settingRep.setMusicSetting(value: newState.isMusicOn);
+    await settingRep.setSoundSetting(value: newState.isSoundOn);
 
     emit(newState);
   }
