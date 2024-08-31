c85442dd62f12b4885d618fb46260a065a4e6765
NonymousMorlock <agamspaul@gmail.com>
2024-08-31 17:43:54 +0000

    🐛 Bug Fix: Add a Unique identifier to the GamePlayState.
    The bloc does not emit a new state when the old state is the same state. When a word is found consecutively, the GamePlayState is emitted with `wordFound` the first time, and the second time around, the emitter doesn't work because the GamePlayState with `wordFound` is still the same as the previous state.

--- lib/src/game_play/bloc/game_play_state.dart
+++ lib/src/game_play/bloc/game_play_state.dart
@@ -1,12 +1,18 @@
 part of 'game_play_bloc.dart';
 
 class GamePlayState extends Equatable {
-  const GamePlayState(this.actualState);
+  GamePlayState(this.actualState) : uniqueIdentifier = const Uuid().v4();
 
   final GamePlayActualState actualState;
+  // When the state goes from wordFound - wordFound, the state is not
+  // changed, this is because the equality is checked by the actualState, in
+  // order to solve this, we can add a unique identifier to the state,
+  // for example, the word that was found, or the letter that was revealed.
+  // or better yet generate a unique identifier for each state.
+  final String uniqueIdentifier;
 
   @override
-  List<Object> get props => [actualState];
+  List<Object> get props => [actualState, uniqueIdentifier];
 }
 
 enum GamePlayActualState {
@@ -21,7 +27,7 @@ enum GamePlayActualState {
 }
 
 class LetterRevealed extends GamePlayState {
-  const LetterRevealed(this.revealedIndex)
+  LetterRevealed(this.revealedIndex)
       : super(GamePlayActualState.letterRevealed);
 
   final int revealedIndex;
