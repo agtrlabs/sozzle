784b39375c1f248cc22937c5b1e1f4956d6ddd4e
NonymousMorlock <agamspaul@gmail.com>
2025-10-24 22:53:22 +0000

    🧪 Test: Initialize Rive for scorecard widget test
    The test for the `Scorecard` widget has been updated to initialize Rive using `setUpAll(RiveNative.init)`. This ensures that the Rive animation framework is ready before the tests run. The test name was also updated for clarity.

--- test/core/common/widgets/scorecard_test.dart
+++ test/core/common/widgets/scorecard_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:rive/rive.dart';
 import 'package:sozzle/core/common/widgets/scorecard.dart';
 import 'package:sozzle/src/theme/cubit/theme_cubit.dart';
 
@@ -9,12 +10,14 @@ import '../../../helpers/helpers.dart';
 void main() {
   late ThemeCubit themeCubit;
 
-  setUp(() async {
+  setUpAll(RiveNative.init);
+
+  setUp(() {
     themeCubit = MockThemeCubit();
     when(() => themeCubit.state).thenReturn(const ThemeStateLight());
   });
 
-  testWidgets('Scorecard', (tester) async {
+  testWidgets('Scorecard finds rive widget', (tester) async {
     await tester.pumpApp(
       BlocProvider.value(
         value: themeCubit,
