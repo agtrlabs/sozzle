970b158ec4534add03271bc611f6d929751ca192
NonymousMorlock <agamspaul@gmail.com>
2025-10-30 23:32:57 +0000

    🗑 Chore: Remove the level_generator package

--- level_generator/android/app/src/main/kotlin/com/example/level_generator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.level_generator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- level_generator/lib/src/rewards_generator/domain/repos/reward_repo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:level_data/level_data.dart';
-import 'package:level_generator/core/error/failure.dart';
-
-abstract class RewardRepo {
-  Future<Either<Failure, List<Reward>>>? getRewards(int? levelId);
-}
