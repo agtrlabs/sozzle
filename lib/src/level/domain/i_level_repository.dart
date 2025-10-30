dfdf7c7e611ae601717f354ccbc6c6fee8bbc69d
NonymousMorlock <agamspaul@gmail.com>
2025-10-30 23:54:09 +0000

    ✨ Feat: Add getLevels method to ILevelRepository
    - Introduced `getLevels()` to the `ILevelRepository` interface. This method will allow fetching a list of all `LevelData` objects.

--- lib/src/level/domain/i_level_repository.dart
+++ lib/src/level/domain/i_level_repository.dart
@@ -4,5 +4,6 @@ import 'package:level_data/level_data.dart';
 // load/save leveldata
 abstract class ILevelRepository {
   Future<LevelData> getLevel(int id);
+  Future<List<LevelData>> getLevels();
   Future<void> setLevel(LevelData level);
 }
